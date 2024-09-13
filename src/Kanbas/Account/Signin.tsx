42e08159f9675f38d1ca8e859cba39b23cd43db0
assignment4 <andreayoung5350@gmail.com>
2024-09-13 16:36:28 -0400

    3.5

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,13 +1,11 @@
-import React from "react";
 import { Link } from "react-router-dom";
-export default function Signup() {
+export default function Signin() {
   return (
-    <div id="wd-signup-screen">
-      <h3>Sign up</h3>
-      <input placeholder="username" /><br/>
-      <input placeholder="password" type="password" /><br/>
-      <input placeholder="verify password" type="password" /><br/>
-      <Link to="/Kanbas/Account/Profile" > Sign up </Link><br />
-      <Link to="/Kanbas/Account/Signin" >Sign in</Link>
+    <div id="wd-signin-screen">
+      <h3>Sign in</h3>
+      <input id="wd-username" placeholder="username" /> <br />
+      <input id="wd-password" placeholder="password" type="password" /> <br />
+      <Link  id="wd-signin-btn" to="/Kanbas/Dashboard"> Sign in </Link> <br />
+      <Link  id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
     </div>
 );}
