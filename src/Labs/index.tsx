2ac738f9396fb10bb774f371db176c718423c0fa
Andrea <andreayoung5350@gmail.com>
2024-09-20 02:41:17 -0400

    added section name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,7 +8,7 @@ export default function Labs() {
     return (
         <div>
             <h1>Labs</h1>
-            <h2>Andrea Lee</h2>
+            <h2>Andrea Lee | CS 4550 01</h2>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
