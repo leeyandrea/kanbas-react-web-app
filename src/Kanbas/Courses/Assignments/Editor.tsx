de3ed6669b56a04f34759b744cab0de62a6410b4
Andrea <andreayoung5350@gmail.com>
2024-09-20 02:25:12 -0400

    final kanbas

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -8,6 +8,7 @@ export default function AssignmentEditor() {
       </textarea>
       <br />
       <table>
+        <br></br>
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-points">Points</label>
@@ -16,6 +17,7 @@ export default function AssignmentEditor() {
             <input id="wd-points" value={100} />
           </td>
         </tr>
+        <br></br>
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-group">Assignment Group</label>
@@ -26,6 +28,7 @@ export default function AssignmentEditor() {
             </select>
           </td>
         </tr>
+        <br></br>
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-display-grade-as">Display Grade as</label>
@@ -36,6 +39,7 @@ export default function AssignmentEditor() {
             </select>
           </td>
         </tr>
+        <br></br>
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-submission-type">Submission Type</label>
@@ -46,30 +50,105 @@ export default function AssignmentEditor() {
             </select>
           </td>
         </tr>
+        <br></br>
         <tr>
+            <td></td>
             <td align="justify" valign="top">
-                <div>
-                    <label>Online Entry Options:</label><br/>
-
-                    <input type="checkbox" name="check-entry" id="wd-text-entry"/>
-                    <label htmlFor="wd-text-entry">Text Entry</label><br/>
-
-                    <input type="checkbox" name="check-entry" id="wd-website-url"/>
-                    <label htmlFor="wd-website-url	">Website URL</label><br/>
-
-                    <input type="checkbox" name="check-entry" id="wd-media-recordings"/>
-                    <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
-
-                    <input type="checkbox" name="check-entry" id="wd-student-annotationy"/>
-                    <label htmlFor="wd-student-annotation">Student Annotations</label>
-
-                    <input type="checkbox" name="check-entry" id="wd-file-upload"/>
-                    <label htmlFor="wd-file-upload">File Uploads</label>
-                </div>
-
+                <label>Online Entry Options:</label><br/>
             </td> 
+             
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <div className="checkbox-group">
+                    <div>
+                        <input type="checkbox" name="check-entry" id="wd-text-entry"/>
+                        <label htmlFor="wd-text-entry">Text Entry</label><br/>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="check-entry" id="wd-website-url"/>
+                        <label htmlFor="wd-website-url	">Website URL</label><br/>
+                    </div>
+                    <div>
+                         <input type="checkbox" name="check-entry" id="wd-media-recordings"/>
+                        <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
+                    </div>
+                    <div>
+                         <input type="checkbox" name="check-entry" id="wd-student-annotationy"/>
+                        <label htmlFor="wd-student-annotation">Student Annotations</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="check-entry" id="wd-file-upload"/>
+                        <label htmlFor="wd-file-upload">File Uploads</label>
+                    </div>         
+                </div>
+            </td>
+        </tr>
+        <br></br>
+        <tr>
+            <td></td>
+            <td align="left" valign="top">
+                <label htmlFor="wd-text-fields-last-name">Assign to:</label>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="right" valign="top">
+                <input type="text" id="wd-assign-to" placeholder="x"
+                value="Everyone" title="Assign to" />
+            </td>
+        </tr>
+        <br></br>
+        <tr>
+            <td></td>
+            <td align="left" valign="top">
+                <label htmlFor="wd-due-date	"> Due: </label>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="left" valign="top">
+                <input type="date"
+                    id="wd-text-fields-due"
+                    value="05/13/2024"/>
+            </td>
+        </tr>
+        <br></br>
+        <tr>
+            <td></td>
+            <td align="left" valign="top">
+                <label htmlFor="wd-available-from"> Available from: </label>
+            </td>
+            <td align="left" valign="top">
+                <label htmlFor="wd-available-until"> Until: </label>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="left" valign="top">
+                <input type="date"
+                    id="wd-available-from"
+                    value="05/06/2024"/>
+            </td>
+            <td align="left" valign="top">
+                <input type="date"
+                    id="wd-available-until"
+                    value="05/20/2024"/>
+            </td>
         </tr>
       </table>
+    <hr />
+    <table align="right">
+        <tr>
+            <td align="right">
+                <button id="wd-cancel">Cancel</button>
+            </td>
+            <td align="right">
+                <button id="wd-save">Save</button>
+            </td>
+        </tr>
+    </table>
     </div>
 );}
 
