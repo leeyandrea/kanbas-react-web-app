3687fa8765fbb6ff8ef245736386ff872719484c
Andrea <andreayoung5350@gmail.com>
2024-09-25 18:50:14 -0400

    3.3.10

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -15,6 +15,8 @@ import ReactIconsSampler from "./ReactIcons";
 import BootstrapGrids from "./BootstrapGrids";
 import ScreenSizeLabel from "./ScreenSizeLabel";
 import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
 export default function Lab2() {
   return (
     <div className="container">
@@ -85,6 +87,8 @@ export default function Lab2() {
       <BootstrapGrids />
       <ScreenSizeLabel />
       <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
     </div>
   );
 }
