40fbdb0af0ba3214ae1f00f015873a17e595066a
Andrea <andreayoung5350@gmail.com>
2024-11-20 19:19:43 -0500

    4.5.3

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -13,10 +13,19 @@ export const updateCourse = async (course: any) => {
     const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
     return data;
 };
+export const createModuleForCourse = async (courseId: string, module: any) => {
+    const response = await axios.post(
+        `${COURSES_API}/${courseId}/modules`,
+        module
+    );
+    return response.data;
+};
+
 export const findModulesForCourse = async (courseId: string) => {
     const response = await axios
         .get(`${COURSES_API}/${courseId}/modules`);
     return response.data;
 };
+
 
 
